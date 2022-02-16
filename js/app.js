19dddf060427359f29f2613856a02a6434e573e4
skakil mia <dewanshakil88@gmail.com>
2022-02-16 22:12:00 +0600

    update all file

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ student.addEventListener("submit",function(e){
     else{
         let arr=[];
         if( getData('apps')){
-            data= getData('apps');
+            arr= getData('apps');
         }
         arr.push({
             name:name.value,
@@ -52,10 +52,7 @@ student.addEventListener("submit",function(e){
         student.querySelector("input[placeholder='Science']").value="";
         student.querySelector("input[placeholder='Religion']").value="";
         allStudentData();
-    }  
-
-   
-      
+    }        
 });
 allStudentData();
 function allStudentData(){
@@ -64,7 +61,7 @@ function allStudentData(){
     let data='';
    
     all_data.map((student, index)=>{
-        data += `
+        data+= `
         <tr>
         <th scope="row">${index+1}</th>                        
         <td>${student.name}</td>
@@ -73,17 +70,96 @@ function allStudentData(){
         <td>${student.gender}</td>
         <td><img style="width:50px; height:50px;object-fit:cover;" src="${ student.photo }"></td>
         <td>3.99</td>
-        <td>A-</td>  
+        <td>A-</td>         
         <td>
-          <button class="bg-info border-0">View</button>   
-          <button class="bg-danger  border-0"">delete</button>   
-        </td>                   
+        <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#studentModal" onclick="single_data(${ index })"  >View</button>
+         <button onclick="deleteStudent(${ index })" class="btn btn-danger btn-sm">Delete</button>
+         </td>                       
       </tr>   
         `;
-    });
-  
- list.innerHTML= data;   
- 
+    });  
+ list.innerHTML= data;  
 
  }
+// delete student id
+function deleteStudent(id){
+    let conf = confirm('Are you sre ?');
+    if(conf){
+        let data = getData('apps');
+        data.splice(id, 1);
+        dataSend('apps',data);
+        allStudentData();
+    }else{
+        return false;
+    }
+}
+const studentModal=document.querySelector(".studentM");
 
+function single_data(index){
+  let res = new  Result;
+
+    let data = getData('apps');
+    studentModal.innerHTML=`
+    <img  src="${data[index].photo}" alt="">
+          <h2>${data[index].name}</h2>          
+            <table class="table ">
+              <thead>
+                <tr>
+                  <td>Subject</td>
+                  <td>Marks</td>
+                  <td>Gpa</td>
+                  <td>Grade</td>
+                  <td>Cgpa</td>
+                  <td>Result</td>
+                </tr>
+              </thead>
+              <tbody>
+                <td>Bangla</td>
+                <td>${data[index].ban}</td>
+                <td>${res.result(data[index].ban).gpacal}</td>
+                <td>A+</td>
+                <td>5</td>
+                <td>A+</td>
+              </tbody>
+              <tbody>
+                <td>English</td>
+                <td>${data[index].en}</td>
+                <td>5</td>
+                <td>A+</td>               
+              </tbody>
+              <tbody>
+                <td>Math</td>
+                <td>${data[index].mat}</td>
+                <td>5</td>
+                <td>A+</td>
+              
+            
+              </tbody>
+              <tbody>
+                <td>Social science</td>
+                <td>${data[index].ss}</td>
+                <td>5</td>
+                <td>A+</td>
+          
+             
+              </tbody>
+              <tbody>
+                <td>Science</td>
+                <td>${data[index].s}</td>
+                <td>5</td>
+                <td>A+</td>
+              
+                
+              </tbody>
+              <tbody>
+                <td>Religion</td>
+                <td>${data[index].reli}</td>
+                <td colspan="6">5</td>
+                <td colspan="6">A+</td>
+ 
+               
+              </tbody>
+            </table>
+    `;   
+
+}
